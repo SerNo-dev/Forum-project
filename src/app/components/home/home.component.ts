61e592696d112955264a76ec44b487620e1d752a
eliaspinna <eliaspinna99@gmail.com>
2024-04-12 13:13:36 +0200

    .

--- src/app/components/home/home.component.ts
+++ src/app/components/home/home.component.ts
@@ -23,6 +23,7 @@ export class HomeComponent implements OnInit {
 
   ngOnInit(): void {
     this.postSrv.getPost().subscribe((data:Post[]) => {
+      
       this.data = data;           
     })
   }
